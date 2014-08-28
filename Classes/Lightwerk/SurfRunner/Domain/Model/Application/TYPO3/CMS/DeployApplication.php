ea28ccccb176d7d4e04d987fa34cde773887a76b
Lars Malach <lp@lightwerk.com>
2014-08-29 00:36:48 +0200

    [TASK] Adds Msql/DumpTask and Mysql/ImportTask to SharedApplication

--- Classes/Lightwerk/SurfRunner/Domain/Model/Application/TYPO3/CMS/DeployApplication.php
+++ Classes/Lightwerk/SurfRunner/Domain/Model/Application/TYPO3/CMS/DeployApplication.php
@@ -17,30 +17,42 @@ use TYPO3\Flow\Annotations as Flow;
 class DeployApplication extends AbstractApplication {
 
 	/**
+	 * 1. initialize: Initialize directories etc. (first time deploy)
+	 * 2. package: Local preparation of and packaging of application assets
+	 * 3. transfer: Transfer of application assets to the node
+	 * 4. update: Update the application assets on the node
+	 * 5. migrate: Migrate (Doctrine, custom)
+	 * 6. finalize: Prepare final release (e.g. warmup)
+	 * 7. test: Smoke test
+	 * 8. switch: Do symlink to current release
+	 * 9. cleanup: Delete temporary files or previous releases
+	 *
 	 * @var array
 	 */
 	protected $tasks = array(
-//		'initialize' => array(),	// Initialize directories etc. (first time deploy)
-		'package' => array(			// Local preparation of and packaging of application assets
+		'initialize' => array(
+			'lightwerk.surftasks:transfer:assureconnection',
+		),
+		'package' => array(
 			'typo3.surf:package:git',
 			'typo3.surf:composer:install',
 		),
-		'transfer' => array(		// Transfer of application assets to the node
+		'transfer' => array(
 			'lightwerk.surftasks:transfer:rsync',
 		),
-//		'update' => array(),		// Update the application assets on the node
-		'migrate' => array(			// Migrate (Doctrine, custom)
+		// 'update' => array(),
+		'migrate' => array(
 			'lightwerk.surftasks:typo3:cms:clearcache',
 			'lightwerk.surftasks:typo3:cms:createuploadfolders',
 			'lightwerk.surftasks:typo3:cms:updatedatabase',
 		),
-		'finalize' => array(		// Prepare final release (e.g. warmup)
+		'finalize' => array(
 			'lightwerk.surftasks:deploymentlog',
 			'lightwerk.surftasks:git:tagnodedeployment',
 		),
-//		'test' => array(),			// Smoke test
-//		'switch' => array(),		// Do symlink to current release
-//		'cleanup' => array(),		// Delete temporary files or previous releases
+		// 'test' => array(),
+		// 'switch' => array(),
+		// 'cleanup' => array(),
 	);
 
 	/**
